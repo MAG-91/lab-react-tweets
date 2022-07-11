86ba36c36f3ef9989fde9cc6c8d1667acad60cc2
MAG <mag_91@hotmail.de>
2022-07-11 15:54:40 +0200

    Done

---
+++ src/components/Actions.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Actions = () => {
+    return (
+
+
+<div className="actions">
+{/* Font Awesome icons */}
+<i class="far fa-comment"></i>
+<i class="fas fa-retweet"></i>
+<i class="far fa-heart"></i>a
+<i class="fas fa-share"></i>
+</div>
+
+)
+}
+
+export default Actions
